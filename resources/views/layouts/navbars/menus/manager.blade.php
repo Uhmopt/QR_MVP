947410478576ae31cb5a8e63ab32308280dfe363
lisovetskyi <lisovetskyi.b.v@gmail.com>
2021-01-15 19:15:03 +0200

    Menu remove

--- resources/views/layouts/navbars/menus/manager.blade.php
+++ resources/views/layouts/navbars/menus/manager.blade.php
@@ -4,12 +4,7 @@
         <a class="nav-link" href="{{ route('branch.edit', auth()->user()->branch->id) }}">
             <i class="ni ni-bullet-list-67 text-blue"></i> {{ __('Branch') }}
         </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('items.index') }}">
-            <i class="ni ni-collection text-pink"></i> {{ __('Menu') }}
-        </a>
-    </li>
+    </li> 
     <li class="nav-item">
         <a class="nav-link" href="{{ route('qr.show', auth()->user()->branch->id) }}">
             <i class="ni ni-mobile-button text-red"></i> {{ __('QR Builder') }}
